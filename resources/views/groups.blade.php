9e6e45822213c4711d37843391916bc70beac336
luizawander <luizasalesgithub@gmail.com>
2026-06-14 11:29:04 -0300

    movendo botões para pagina de grupo

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout title="Grupos" active="groups">
-    <x-page-title>Todos os grupos</x-page-title>
+    <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
+        <x-page-title class="!mb-0">Todos os grupos</x-page-title>
+        <div class="flex flex-wrap items-center gap-3 lg:absolute lg:top-[130px] lg:right-[120px]">
+            <x-button variant="pastel-green" :full="false" size="sm">
+                Convites pendentes
+            </x-button>
+            <x-button variant="gold" :full="false" size="sm">
+                + Novo grupo
+            </x-button>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,5 @@
 <x-app-layout title="Início" active="home">
     <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
         <x-page-title class="!mb-0">Painel geral</x-page-title>
-        <div class="flex flex-wrap items-center gap-3 lg:absolute lg:top-[130px] lg:right-[120px]">
-            <x-button variant="pastel-green" :full="false" size="sm">
-                Convites pendentes
-            </x-button>
-            <x-button variant="gold" :full="false" size="sm">
-                + Novo grupo
-            </x-button>
-        </div>
     </div>
 </x-app-layout>
